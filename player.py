7965e780ccc8bafbd2b3bad3a9825dc9df7ef21a
Jakub Pietrzak <kubapietrzak7@gmail.com>
2024-07-31 15:38:43 +0200

    Defined init_players_evolution

--- player.py
+++ player.py
@@ -2,7 +2,7 @@ class Player:
     """
     Player class
     """
-    def __init__(self, strategy, name: str):
+    def __init__(self, strategy, name: str, score: int = 0):
         """
         Constructor for the player class
         :param strategy: Function pointer to the strategy function.
@@ -12,7 +12,8 @@ class Player:
             Outputs:
                 boolean decision (true - cooperate, false - deflect)
         :param name: Unique name, constructed as <strategy_name> + #num (for #1, the #1 is omitted)
+        :param score: Starting score of the player. Usually 0. Starting population size for the evolution simulation
         """
         self.strategy = strategy
-        self.score: int = 0
+        self.score: int = score  # For the evolution simulation - population size
         self.name: str = name
